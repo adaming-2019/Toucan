5cfb12ef77f137cf0f2834ab45520a6fc0a4d19a
IN-BR-002 <IN-BR-002@IN-BR-002>
2019-12-20 11:06:28 +0100

    Emilie : vues et controller pour liste agences et assurances

---
+++ BoVoyage_Toucan/src/main/java/fr/adaming/controllers/AgenceController.java
@@ -0,0 +1,39 @@
+package fr.adaming.controllers;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import fr.adaming.entities.Agence;
+import fr.adaming.service.IAgenceService;
+
+@Controller
+public class AgenceController {
+	// Transformation de l'association uml en java
+	@Autowired
+	private IAgenceService agenceService;
+
+	// Le setter pour l'injection dépendance
+	public void setAgenceService(IAgenceService agenceService) {
+		this.agenceService = agenceService;
+	}
+
+	@PostConstruct
+	public void init() {
+
+	}
+
+	@RequestMapping(value = "/listeAgence", method = RequestMethod.GET)
+	public ModelAndView afficheListe() {
+		// recup de la liste des agence du formateur
+		List<Agence> listeAgence = agenceService.getAllAgence();
+		return new ModelAndView("listeAgenceAdm", "agences", listeAgence);
+	}
+
+}
---
+++ BoVoyage_Toucan/src/main/java/fr/adaming/controllers/AssuranceController.java
@@ -0,0 +1,39 @@
+package fr.adaming.controllers;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import fr.adaming.entities.Assurance;
+import fr.adaming.service.IAssuranceService;
+
+@Controller
+public class AssuranceController {
+	// Transformation de l'association uml en java
+	@Autowired
+	private IAssuranceService AssuranceService;
+
+	// Le setter pour l'injection dépendance
+	public void setAssuranceService(IAssuranceService AssuranceService) {
+		this.AssuranceService = AssuranceService;
+	}
+
+	@PostConstruct
+	public void init() {
+
+	}
+
+	@RequestMapping(value = "/listeAssurance", method = RequestMethod.GET)
+	public ModelAndView afficheListe() {
+		// recup de la liste des Assurance du formateur
+		List<Assurance> listeAssurance = AssuranceService.getAllAssurances();
+		return new ModelAndView("listeAssuranceAdm", "Assurances", listeAssurance);
+	}
+
+}
