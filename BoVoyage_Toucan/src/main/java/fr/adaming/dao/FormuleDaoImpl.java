ef3fa989559c856ab2d218a2183e7703167d9a67
IN-BR-004 <IN-BR-004@IN-BR-004>
2019-12-20 12:03:17 +0100

    ddddd

--- BoVoyage_Toucan/src/main/java/fr/adaming/dao/FormuleDaoImpl.java
+++ BoVoyage_Toucan/src/main/java/fr/adaming/dao/FormuleDaoImpl.java
@@ -8,6 +8,7 @@ import fr.adaming.entities.Formule;
 @Repository
 public class FormuleDaoImpl extends GenericDaoImpl<Formule> implements IFormuleDao{
 
+	
 	// specifier le vrai type de T (la vraie Entity à utiliser ) ici c'est Prestation 
 	public FormuleDaoImpl() {
 		clazz=Formule.class;
