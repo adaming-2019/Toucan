f72c5413c3b350732d9368baf14bc6fb924bb214
giacomopinna <IN-BR-011@IN-BR-011>
2019-12-28 11:18:32 +0100

    Giacomo : tentative de modification controlleur pour résoudre une erreur

--- BoVoyage_Toucan/src/main/java/fr/adaming/controllers/ContinentPaysControleur.java
+++ BoVoyage_Toucan/src/main/java/fr/adaming/controllers/ContinentPaysControleur.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Lob;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
